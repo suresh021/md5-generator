c5bc6db6cacfcc7f6e7ac1d330a54847b535212c
Suresh Pokharel OFFICE <suresh.wrc@gmail.com>
2017-06-29 17:16:09 +0545

    lint corrections

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function convert(){
+	var plain_string = $( "#txt_input" ).val();
+	$.ajax({
+		url:'process.php?plain_string=' + plain_string,
+		complete: function (response) {
+     		var status = JSON.parse(response.responseText);//parsing status from response received from php
+     		$('#txt_output').val(status.md5);
+        },
+     	error: function () {
+     		$('#txt_output').val('Bummer: there was an error!');
+     	},
+    });
+}
--- process.php
+++ process.php
@@ -1,9 +1,7 @@
 <?php 
 	if (isset($_GET['plain_string'])) {
 		$string=$_GET['plain_string'];
-  		
-  			$response_array['md5']=md5($string);
-
+  		$response_array['md5']=md5($string);
   		header('Content-type: application/json');//preparing correct format for json_encode
     	echo json_encode($response_array);//sending response to ajax
 	}
